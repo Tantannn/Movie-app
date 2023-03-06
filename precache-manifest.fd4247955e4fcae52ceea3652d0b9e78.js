928d8cdf61859e1e9f5da55b2fb86f2dd3f8b121
Tantannn <tango.11@yahoo.com>
2023-03-06 16:09:41 +0700

    Updates

---
+++ precache-manifest.fd4247955e4fcae52ceea3652d0b9e78.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "700ec0dd817095710ec2d08cc1c42658",
+    "url": "/Movie-app/index.html"
+  },
+  {
+    "revision": "d3b4a46787055cd8abd9",
+    "url": "/Movie-app/static/css/main.0eed716d.chunk.css"
+  },
+  {
+    "revision": "f1d2feb58ed56a1f8bea",
+    "url": "/Movie-app/static/js/2.af4053d9.chunk.js"
+  },
+  {
+    "revision": "894f61387b383e6e2e0543fb120b4656",
+    "url": "/Movie-app/static/js/2.af4053d9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d3b4a46787055cd8abd9",
+    "url": "/Movie-app/static/js/main.a34581a1.chunk.js"
+  },
+  {
+    "revision": "22c7bb36030bb84cd132",
+    "url": "/Movie-app/static/js/runtime-main.0cf6dfbc.js"
+  }
+]);
